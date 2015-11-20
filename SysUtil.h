d6b6ff4c3bcfea98d48643938db855843a748c05
fuckgfwo <a@b.com>
2015-11-20 23:56:33 +0800

    first commit

---
+++ SysUtil.h
@@ -0,0 +1,14 @@
+//
+//  SysUtil.h
+//  SimpleTunnel
+//
+//  Created by network extension on 15/10/21.
+//  Copyright © 2015年 Apple Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <resolv.h>
+#include <dns_sd.h>
+@interface SysUtil : NSObject
++ (NSString*)loadSystemDNSServer;
+@end
